0be58c9dfafe5038098163545b78e1e0c7166abb
rohola <hilbert.cantor@gmail.com>
2019-02-18 09:50:02 -0700

    changes to work on server

--- run_classifier.sh
+++ run_classifier.sh
@@ -1,10 +1,14 @@
 
 export CUDA_VISIBLE_DEVICES=2
 
-python run_classifier.py --task_name=MRPC \
+
+
+
+
+python run_classifier.py --task_name=COLA \
 --do_train=true \
 --do_eval=true \
---data_dir=/home/rohola/codes/bert/glue_data/MRPC \
+--data_dir=/home/rohola/codes/bert/glue_data/CoLA \
 --vocab_file=/home/rohola/codes/bert/uncased_L-12_H-768_A-12/vocab.txt \
 --bert_config_file=/home/rohola/codes/bert/uncased_L-12_H-768_A-12/bert_config.json \
 --init_checkpoint=/home/rohola/codes/bert/uncased_L-12_H-768_A-12/bert_model.ckpt \
@@ -12,4 +16,5 @@ python run_classifier.py --task_name=MRPC \
 --train_batch_size=32 \
 --learning_rate=2e-5 \
 --num_train_epochs=3.0 \
---output_dir=/home/rohola/codes/outputs/mrpc_output/
+--output_dir=/home/rohola/codes/outputs/cola_output/
+
