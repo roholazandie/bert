c566eac47222158c14f46d89e7d2400176fac2c1
rohola <hilbert.cantor@gmail.com>
2019-03-04 10:25:08 -0700

    edited run_create_pretraining.sh

--- run_create_pretraining_data.sh
+++ run_create_pretraining_data.sh
@@ -3,13 +3,12 @@
 
 
 
-python create_pretraining_data.py
---input_file=/media/data/rohola_data/cleaned_wiki.txt
---output_file=/media/data/rohola_data/tf_wiki.tfrecord
---vocab_file=/home/rohola/codes/bert/uncased_L-12_H-768_A-12/vocab.txt
---do_lower_case=True
---max_seq_length=128
---max_predictions_per_seq=20
---masked_lm_prob=0.15
---random_seed=12345
+python create_pretraining_data.py --input_file=/media/data/rohola_data/cleaned_wiki.txt \
+--output_file=/media/data/rohola_data/tf_wiki.tfrecord \
+--vocab_file=/home/rohola/codes/bert/uncased_L-12_H-768_A-12/vocab.txt \
+--do_lower_case=True \
+--max_seq_length=128 \
+--max_predictions_per_seq=20 \
+--masked_lm_prob=0.15 \
+--random_seed=12345 \
 --dupe_factor=5
